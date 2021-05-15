e96576b115c2c6a6d3282a9a36db393e9408a40a
taserbeat <taserbeat@gmail.com>
2021-05-16 02:07:28 +0900

    アクションテスト10-コミット追加2

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            GitHub add commit
+            GitHub add commit2
           </a>
           <span>, </span>
           <a
