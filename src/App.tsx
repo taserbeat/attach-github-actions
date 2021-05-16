4a1ba6c527384b6d443f89013add0d0de9e14434
taserbeat <taserbeat@gmail.com>
2021-05-16 17:16:34 +0900

    アクションテスト13

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            masterへ直接プッシュ
+            アクションテスト13
           </a>
           <span>, </span>
           <a
